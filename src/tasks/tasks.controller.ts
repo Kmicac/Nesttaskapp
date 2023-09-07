59186cd62189df23c4bc7be69d4a4d336994a42c
Camilo <camilojpg30@gmail.com>
2023-09-07 17:30:19 -0300

    DTO created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,12 +12,18 @@ export class TasksController {
         return this.tasksService.getAllTasks();
     }
 
-    @Post()
-    createTask(
-        @Body('title') title: string,
-        @Body('description') description: string 
-        ): Task {
+    // @Post()
+    // createTask(
+    //     @Body('title') title: string,
+    //     @Body('description') description: string 
+    //     ): Task {
 
-        return this.tasksService.createTask(title, description);
+    //     return this.tasksService.createTask(title, description);
+    // }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+
+        return this.tasksService.createTask(createTaskDto);
     }
 }
